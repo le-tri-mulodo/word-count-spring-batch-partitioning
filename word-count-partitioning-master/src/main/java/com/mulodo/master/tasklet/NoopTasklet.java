a2ee631a439e5addc79a0ff79f748cec26b0a45e
Tri Le <le.tri@mulodo.com>
2015-04-20 10:13:28 +0700

    Init project

---
+++ word-count-partitioning-master/src/main/java/com/mulodo/master/tasklet/NoopTasklet.java
@@ -0,0 +1,22 @@
+package com.mulodo.master.tasklet;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+/**
+ * <p>
+ * No-op {@link org.springframework.batch.core.step.tasklet.Tasklet}.
+ * </p>
+ */
+public class NoopTasklet implements Tasklet
+{
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+            throws Exception
+    {
+        return RepeatStatus.FINISHED;
+    }
+}
---
+++ word-count-partitioning-slave/src/main/java/com/mulodo/slave/JobRunner.java
@@ -0,0 +1,18 @@
+package com.mulodo.slave;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * <p>
+ * Job runner simply which loads the context for the slave nodes.
+ * </p>
+ */
+public class JobRunner
+{
+    private static final String CONFIG = "/META-INF/spring/config/job-context.xml";
+
+    public static void main(String[] args) throws Exception
+    {
+        new ClassPathXmlApplicationContext(CONFIG);
+    }
+}
